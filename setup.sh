d2a86fd7377d366f6706df09a4bc848e7617c637
zittoooo <stellar7484@naver.com>
2021-04-16 14:12:45 +0900

    ADD static ip

--- setup.sh
+++ setup.sh
@@ -9,4 +9,49 @@ kubectl apply -f ./srcs/metallb/metallb_config.yaml
 cd ./srcs/nginx
 echo "nginx image build"
 docker build -t nginx:latest . > /dev/null
+echo "apply yaml"
 kubectl apply -f ./nginx.yaml
+
+#mysql
+cd ../mysql
+echo "mysql image build"
+docker build -t mysql:latest . > /dev/null
+echo "apply yaml"
+kubectl apply -f mysql.yaml
+
+#phpmyadmin
+cd ../phpmyadmin
+echo "phpmyadmin image build"
+docker build -t phpmyadmin:latest . > /dev/null
+echo "apply yaml"
+kubectl apply -f phpmyadmin.yaml
+
+#wordpress
+# cd ../wordpress
+# echo "wordpress image build"
+# docker build -t wordpress:latest . > /dev/null
+# kubectl apply -f wordpress.yaml
+
+# ftps
+# cd ../ftps
+# echo "ftpss image build"
+# docker build -t ftps:latest . > /dev/null
+# kubectl apply -f ftps.yaml
+
+# influxdb
+# cd ../influxdb
+# echo "influxdb image build"
+# docker build -t influxdb:latest . > /dev/null
+# kubectl apply -f influxdb.yaml
+
+# telegraf
+# cd ../telegraf
+# echo "telegraf image build"
+# docker build -t telegraf:latest . > /dev/null
+# kubectl apply -f telegraf.yaml
+
+# grafana
+# cd ../grafana
+# echo "grafana image build"
+# docker build -t grafana:latest . > /dev/null
+# kubectl apply -f grafana.yaml
